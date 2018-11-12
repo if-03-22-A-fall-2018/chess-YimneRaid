9415e31665392598dbaa24bc0c7f1e3a8b778b7a
Yimne <yimne@Yimne.Raid>
2018-11-12 19:21:31 +0100

    A

--- assignment/chess.h
+++ assignment/chess.h
@@ -3,10 +3,25 @@
  * ---------------------------------------------------------
  * Exercise Number: 0
  * Title:			chess.h
- * Author:			P. Bauer
+ * Author:		Yimne Raid
  * Due Date:		October 15, 2014
  * ----------------------------------------------------------
  * Description:
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+ bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type);
+ void 	init_chess_board (ChessBoard chess_board);
+ struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank);
+ bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank);
+ bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece);
+ struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank);
+ void 	setup_chess_board (ChessBoard chess_board);
+ bool 	remove_piece (ChessBoard chess_board, File file, Rank rank);
+ bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+ bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
