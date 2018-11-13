b9c6d5a57f7986d7526ed614b9aa0eae4e3e8088
Yimne <yimne@Yimne.Raid>
2018-11-13 11:30:37 +0100

    header

--- assignment/chess.h
+++ assignment/chess.h
@@ -4,17 +4,12 @@
  * Exercise Number: 0
  * Title:			chess.h
  * Author:		Yimne Raid
-<<<<<<< HEAD
  * Due Date:	12.Nov, 2018
-=======
- * Due Date:		October 15, 2014
->>>>>>> 7fdbd46114c6bbe77ac7fb5e4b1503257ad8ba55
  * ----------------------------------------------------------
  * Description:
  * Basic chess functions.
  * ----------------------------------------------------------
  */
-<<<<<<< HEAD
 
 
  //Enumerations
@@ -57,47 +52,3 @@ bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File 
 bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
 bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
 #endif
-=======
- bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type){
-   return false;
- };
- void 	init_chess_board (ChessBoard chess_board){
-   return false;
- };
- struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank)
-
- bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank){
-   return false;
- };
- bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece){
-   return false;
- };
- struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank)
- void 	setup_chess_board (ChessBoard chess_board){
-   return false;
- };
- bool 	remove_piece (ChessBoard chess_board, File file, Rank rank){
-   return false;
- };
- bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
- bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r){
-   return false;
- };
->>>>>>> 7fdbd46114c6bbe77ac7fb5e4b1503257ad8ba55
